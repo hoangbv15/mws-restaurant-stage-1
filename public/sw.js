aa283bef4738712d90c48364e752f9971d115a1f
Hoang Vu Bui <buivuhoang15@gmail.com>
2018-04-02 17:21:51 +0100

    feat: add sw to support offline first

---
+++ public/sw.js
@@ -0,0 +1,79 @@
+var staticCacheName = 'mws-restaurant-static-v1';
+var contentImgsCache = 'mws-restaurant-content-imgs';
+var allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
+
+self.addEventListener('install', function(event) {
+  console.log("Service worker installed!");
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/js/main.js', '/js/dbhelper.js',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        //'imgs/icon.png',
+        //'https://normalize-css.googlecode.com/svn/trunk/normalize.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant-') &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+      event.respondWith(caches.match('/index.html'));
+      return;
+    }
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+function servePhoto(request) {
+  var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
+
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      let fetchedResult = fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+
+      if (response) return response;
+      return fetchedResult;
+    });
+  });
+}
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
